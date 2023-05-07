1e1741a126e27fad3d4ac23681784fb80239a577
eacarrasco <113465972+eacarrasco@users.noreply.github.com>
2023-05-07 13:33:36 -0700

    Update linkedlist.cpp

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -25,19 +25,7 @@ bool linked_list::isEmpty() const {
 }
 
 unsigned linked_list::listSize() const {
-    if(!head){
-        return 0;
-    }
-    
-    game *runner = head;
-    int count = 1;
-
-    while(runner != tail){
-        runner = runner->next;
-        count++;
-    }
-
-    return count;
+    return length;
 }
 
 game& linked_list::operator[](int n) {
@@ -57,4 +45,32 @@ void linked_list::append(game newgame) {
     }
     tail->setId(length);
     length++;
+}
+
+void linked_list::remove(int r) {
+    if (r > length) return;
+   
+    game* runner = head;
+    game* prev;
+    for (int i = 0; i < r; i++) {
+        prev = runner;
+        runner = runner->next;
+    }
+    
+    if (runner == head) {
+        if (head == tail) tail = nullptr;
+        head = head->next;
+        delete runner;
+        return;
+    }
+    
+    if (runner == tail) {
+        prev->next = nullptr;
+        delete runner;
+        tail = prev;
+        return;
+    }
+    
+    prev->next = runner->next;
+    delete runner;
 }
