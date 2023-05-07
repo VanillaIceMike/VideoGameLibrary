f142e340bfd18295b19873e5dee4c8b8f2e93b7f
eacarrasco <113465972+eacarrasco@users.noreply.github.com>
2023-05-07 13:07:18 -0700

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -6,8 +6,9 @@ game::game(){
 }
 
 game::game(std::string s1, float f1, gameSpec spec){
-    for (int i = 0; i < s1.length(); i++) s1[i] = tolower(s1[i]);
     gameName = s1;
+    for (int i = 0; i < s1.length(); i++) s1[i] = tolower(s1[i]);
+    searchName = s1;
     price = f1;
 
     specs = spec;
@@ -15,14 +16,16 @@ game::game(std::string s1, float f1, gameSpec spec){
 
 game::game(const game& newgame) {
     gameName = newgame.getName();
+    searchName = newgame.getSearchName();
     price = newgame.getPrice();
 
-    specs = newgame.specs;
+    specs = newgame.getSpecs();
 }
 
 void game::setName(std::string name){
-    for (int i = 0; i < name.length(); i++) name[i] = tolower(name[i]);
     gameName = name;
+    for (int i = 0; i < name.length(); i++) name[i] = tolower(name[i]);
+    searchName = name;
 }
 
 void game::setPrice(float p){
@@ -39,4 +42,12 @@ std::string game::getName() const{
 
 float game::getPrice() const{
     return price;
+}
+
+gameSpec game::getSpecs() const {
+    return specs;
+}
+
+std::string getSearchName() const {
+    retrun searchName;
 }
